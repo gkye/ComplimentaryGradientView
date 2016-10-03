9f30e1fafc4d46230fa6d9cd1cc2ffbc22587f86
= <qwstnz@gmail.com>
2016-10-03 10:12:04 -0400

    Added more gradient start point options

--- Source/GradientStartPoint.swift
+++ Source/GradientStartPoint.swift
@@ -11,21 +11,34 @@ import Foundation
 public enum GradientStartPoint: String{
   
   case top
+  case topRight
+  case topLeft
   case right
   case left
   case bottom
+  case bottomRight
+  case bottomLeft
+  
   
   mutating func startPointEnumFromString(_ storyboardValue: String){
     let value = storyboardValue.lowercased()
     switch value {
     case GradientStartPoint.top.rawValue:
       self = .top
+    case GradientStartPoint.topRight.rawValue:
+      self = .topRight
+    case GradientStartPoint.topLeft.rawValue:
+      self = .topLeft
     case GradientStartPoint.right.rawValue:
       self = .right
     case GradientStartPoint.left.rawValue:
       self = .left
     case GradientStartPoint.bottom.rawValue:
       self = .bottom
+    case GradientStartPoint.bottomRight.rawValue:
+      self = .bottomRight
+    case GradientStartPoint.bottomLeft.rawValue:
+      self = .bottomLeft
     default:
       self = .top
     }
