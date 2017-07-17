1f20633c6332c090f33f9ab7361809ac1c23b79a
Ilya Seliverstov <ilya@amudo.com>
2017-07-17 17:33:39 +0200

    fixed performance issues: + avoid recalculate gradient colours everiytime on layout + avoid to resize images which width is lower that 250px
    now gradient layer is always insert as background layer
    
    added gitignore

--- Source/ComplimentaryGradientView.swift
+++ Source/ComplimentaryGradientView.swift
@@ -9,81 +9,85 @@ import Foundation
 import UIKit
 
 @objc public protocol ComplimentaryGradientViewDelegate: class {
-  @objc optional func complimentaryGradientView(didSetGradient gradientView: ComplimentaryGradientView, gradientSet: Bool)
+    @objc optional func complimentaryGradientView(didSetGradient gradientView: ComplimentaryGradientView, gradientSet: Bool)
 }
 
 public typealias CustomPoints = (startPoint: CGPoint, endPoint: CGPoint)
 
 @IBDesignable
 open class ComplimentaryGradientView: UIView{
-  
-  //MARK: public
-  
-  fileprivate let gradientLayer: CAGradientLayer! = CAGradientLayer()
-  
-  
-  /// Gradients are created from colors are retrived from this image
-  @IBInspectable open var image: UIImage?{
-    didSet{
-      if let img = image{
-        configGradient(img)
-      }
-    }
-  }
-  
-  @IBInspectable var type: String! = "backgroundPrimary"{
-    didSet{
-      gradientType.gradientTypeFromString(type)
-    }
-  }
-  
-  @IBInspectable var startPoint: String! = "top"{
-    didSet{
-      gradientStartPoint.startPointEnumFromString(startPoint)
-    }
-  }
-  
-  open var gradientType: GradientType = .backgroundPrimary
-  open var gradientStartPoint: GradientStartPoint = .top
-  
-  open var customPoint: CustomPoints?
-  
-  /// Executed fetching of image colors in background if set true. Default = false
-  open var backgroundExecution: Bool = false
-  
-  open weak var delegate: ComplimentaryGradientViewDelegate?
-  
-  //MARK: private
-  
-  open override func layoutSubviews() {
-    super.layoutSubviews()
-    if let img = image{
-      configGradient(img)
-    }
-  }
-  
-  
-  fileprivate func configGradient(_ image: UIImage){
-    if !backgroundExecution{
-      let imageColors = image.getColors()
-      configGradientWithStartColor(imageColors)
-    }else{
-      image.getColors{
-        colors in
-        self.configGradientWithStartColor(colors)
-      }
-    }
-  }
-  
-  fileprivate func configGradientWithStartColor(_ imageColors: UIImageColors) {
-    let gradientColors = gradientType.getGradientColors(imageColors)
     
-    gradientLayer.removeFromSuperlayer()
-    gradientLayer.frame.size = self.frame.size
-    gradientLayer.colors = gradientColors
-    gradientLayer.addStartPointsToGradient(gradientStartPoint, customPoint: customPoint)
-    self.layer.addSublayer(gradientLayer)
-    delegate?.complimentaryGradientView?(didSetGradient: self, gradientSet: true)
-  }
-  
+    //MARK: public
+    
+    fileprivate let gradientLayer: CAGradientLayer! = CAGradientLayer()
+    fileprivate var calculatedColor = [CGColor]()
+    
+    /// Gradients are created from colors are retrived from this image
+    @IBInspectable open var image: UIImage?{
+        didSet{
+            if let img = image{
+                configGradient(img)
+            }
+        }
+    }
+    
+    @IBInspectable var type: String! = "backgroundPrimary"{
+        didSet{
+            gradientType.gradientTypeFromString(type)
+        }
+    }
+    
+    @IBInspectable var startPoint: String! = "top"{
+        didSet{
+            gradientStartPoint.startPointEnumFromString(startPoint)
+        }
+    }
+    
+    open var gradientType: GradientType = .backgroundPrimary
+    open var gradientStartPoint: GradientStartPoint = .top
+    
+    open var customPoint: CustomPoints?
+    
+    /// Executed fetching of image colors in background if set true. Default = false
+    open var backgroundExecution: Bool = false
+    
+    open weak var delegate: ComplimentaryGradientViewDelegate?
+    
+    //MARK: private
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        if !calculatedColor.isEmpty {
+            applyGradient() // apply already calculated colors
+        } else
+        if let img = image{
+            configGradient(img)
+        }
+    }
+    
+    fileprivate func configGradient(_ image: UIImage){
+        let size = image.size.width > 250.0 ? CGSize.zero : image.size // avoid upscale if image less that 250px width
+        if !backgroundExecution{
+            configGradientWithStartColor(image.getColors(scaleDownSize: size))
+        }else{
+            image.getColors(scaleDownSize: size) { [weak self] colors in
+                self?.configGradientWithStartColor(colors)
+            }
+        }
+    }
+    
+    fileprivate func configGradientWithStartColor(_ imageColors: UIImageColors) {
+        calculatedColor = gradientType.getGradientColors(imageColors)
+
+        gradientLayer.removeFromSuperlayer()
+        applyGradient()
+        self.layer.insertSublayer(gradientLayer, at: 0) // always will be as background layer
+        delegate?.complimentaryGradientView?(didSetGradient: self, gradientSet: true)
+    }
+
+    fileprivate func applyGradient() {
+        gradientLayer.frame.size = self.frame.size
+        gradientLayer.colors = calculatedColor
+        gradientLayer.addStartPointsToGradient(gradientStartPoint, customPoint: customPoint)
+    }
 }
